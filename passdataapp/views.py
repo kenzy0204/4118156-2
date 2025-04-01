e17fb85bc0758da9dbe05eb8b7f0616caaf2754d
kris <146816667+kenzy0204@users.noreply.github.com>
2025-04-01 15:45:39 +0800

    Update views.py

--- passdataapp/views.py
+++ passdataapp/views.py
@@ -40,6 +40,6 @@ def filter(request):
 	return render(request,"filter.html",locals())    
 
 def newDocument(response):
-   return HttpResponse('<h1>學號:41118112</h1><br><h1>姓名:邱勻誌</h1>')
+   return HttpResponse('<h1>學號:41118156</h1><br><h1>黃俊傑黃俊傑</h1>')
 
 
